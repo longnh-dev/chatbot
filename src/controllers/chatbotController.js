68867d679c722532f7feb6e874aef2e90ad4d53d
hoanglong <hoanglongcnttk42b@gmail.com>
2021-06-13 01:48:36 +0700

    upload

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -69,29 +69,64 @@ let getWebhook = (req, res) => {
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
-
     let response;
 
-    // Check if the message contains text
+    // Checks if the message contains text
     if (received_message.text) {
-        console.log("===========================")
-        console.log("received_message.text")
-        console.log("===========================")
-
-
-
-        // Create the payload for a basic text message
+        // Create the payload for a basic text message, which
+        // will be added to the body of our request to the Send API
         response = {
-            "text": `Bạn đã gửi một tin nhắn là: "${received_message.text}". Bây giờ hãy gửi cho mình 1 cái ảnh!`
+            "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+        }
+    } else if (received_message.attachments) {
+        // Get the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Bạn muốn có muốn nhận tư vấn về mặt hàng này không ạ ?",
+                        "subtitle": "Vui lòng nhấn câu trả lời bên dưới để được tư vấn !",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Có",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "Không",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
         }
     }
 
-    // Sends the response message
+    // Send the response message
     callSendAPI(sender_psid, response);
 }
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
+    let response;
+
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+        response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+        response = { "text": "Oops, try sending another image." }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
 
 }
 
